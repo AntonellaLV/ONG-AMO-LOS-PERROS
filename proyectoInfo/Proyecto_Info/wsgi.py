d9f6c9d7f0220fc0605342d88422a1a248a6bc7e
AntonellaLV <118840064+AntonellaLV@users.noreply.github.com>
2022-12-26 05:26:48 -0300

    Update wsgi.py

--- proyectoInfo/Proyecto_Info/wsgi.py
+++ proyectoInfo/Proyecto_Info/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoInfo11.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoInfo11.settings')
 
 application = get_wsgi_application()
